f964abfe3f2773d808c4a5929d9011a54544d177
Willem Sonke <willem@wimiso.nl>
2020-05-05 17:24:03 +0200

    Add pronoun helper function
    Forgot to commit this earlier...

---
+++ pronouns.js
@@ -0,0 +1,62 @@
+module.exports = {
+	getConjugatedForms: getConjugatedForms
+}
+
+function getConjugatedForms(dictionary) {
+	let forms = {};
+
+	let plurals = ["", "me", "pxe", "ay"];
+	let cases = ["", "l", "t", "r", "ä", "ri"];
+
+	for (let w in dictionary) {
+		if (dictionary.hasOwnProperty(w)) {
+			let word = dictionary[w];
+			if (word["type"] === "pn" && word["conjugation"]) {
+				for (let i = 0; i < 4; i++) {
+					for (let j = 0; j < 6; j++) {
+						let form = word["conjugation"]["forms"][i][j];
+						let allForms = formsFromString(form);
+						for (let f in allForms) {
+							if (allForms.hasOwnProperty(f)) {
+								forms[allForms[f]] = {
+									"word": word,
+									"plural": plurals[i],
+									"case": cases[j]
+								};
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	return forms;
+}
+
+function formsFromString(formString) {
+	if (!formString) {
+		return [];
+	}
+	let forms = [];
+
+	let split = formString.split(";");
+	for (let i = 0; i < split.length; i++) {
+		forms = forms.concat(formsRecursive(split[i]));
+	}
+	return forms;
+}
+
+function formsRecursive(formString) {
+	let forms = [];
+	let regex = /([^(]*)\(([^)]*)\)(.*)/;
+	let result = regex.exec(formString);
+	if (result === null) {
+		forms.push(formString.split("-").join(""));
+	} else {
+		forms = forms.concat(formsRecursive(result[1] + result[2] + result[3]));
+		forms = forms.concat(formsRecursive(result[1] + result[3]));
+	}
+	return forms;
+}
+
