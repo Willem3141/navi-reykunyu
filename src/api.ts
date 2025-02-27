Willem3141/navi-reykunyu

commit d3565565f2319151f416b7d2db570d7093df029a

Make the Zeykerokyu functions async
Also, redo the server routing code to be more consistent.

src/api.ts
@@ -231,20 +231,26 @@ router.post('/user/unmark-favorite',
 
 router.get('/srs/courses',
 	checkLoggedIn(),
-	(req, res) => {
-		zeykerokyu.getCourses((courses: Course[]) => {
+	async (req, res, next) => {
+		try {
+			const courses = await zeykerokyu.getCourses();
 			res.json(courses);
-		});
+		} catch (e) {
+			next(e);
+		}
 	}
 );
 
 router.get('/srs/lessons',
 	checkLoggedIn(),
 	parseIntegerParameter('courseId', 'get'),
-	(req, res) => {
-		zeykerokyu.getLessons(req.user!, req.args!['courseId'] - 1, (lessons: Lesson[]) => {
+	async (req, res, next) => {
+		try {
+			const lessons = await zeykerokyu.getLessons(req.user!, req.args!['courseId'] - 1);
 			res.json(lessons);
-		});
+		} catch (e) {
+			next(e);
+		}
 	}
 );
 
@@ -252,10 +258,13 @@ router.get('/srs/lesson',
 	checkLoggedIn(),
 	parseIntegerParameter('courseId', 'get'),
 	parseIntegerParameter('lessonId', 'get'),
-	(req, res) => {
-		zeykerokyu.getLessonData(req.args!['courseId'] - 1, req.args!['lessonId'] - 1, (lessons: Lesson) => {
+	async (req, res, next) => {
+		try {
+			const lessons = await zeykerokyu.getLessonData(req.args!['courseId'] - 1, req.args!['lessonId'] - 1);
 			res.json(lessons);
-		});
+		} catch (e) {
+			next(e);
+		}
 	}
 );
 
@@ -263,10 +272,13 @@ router.get('/srs/items',
 	checkLoggedIn(),
 	parseIntegerParameter('courseId', 'get'),
 	parseIntegerParameter('lessonId', 'get'),
-	(req, res) => {
-		zeykerokyu.getItemsForLesson(req.args!['courseId'] - 1, req.args!['lessonId'] - 1, req.user!, (items: LearnableItem[]) => {
+	async (req, res, next) => {
+		try {
+			const items = await zeykerokyu.getItemsForLesson(req.args!['courseId'] - 1, req.args!['lessonId'] - 1, req.user!);
 			res.json(items);
-		});
+		} catch (e) {
+			next(e);
+		}
 	}
 );
 
@@ -274,51 +286,52 @@ router.get('/srs/learnable',
 	checkLoggedIn(),
 	parseIntegerParameter('courseId', 'get'),
 	parseIntegerParameter('lessonId', 'get'),
-	(req, res) => {
-		zeykerokyu.getLearnableItemsForLesson(req.args!['courseId'] - 1, req.args!['lessonId'] - 1, req.user!, (items: LearnableItem[]) => {
+	async (req, res, next) => {
+		try {
+			const items = zeykerokyu.getLearnableItemsForLesson(req.args!['courseId'] - 1, req.args!['lessonId'] - 1, req.user!);
 			res.json(items);
-		});
+		} catch (e) {
+			next(e);
+		}
 	}
 );
 
 router.get('/srs/reviewable',
 	checkLoggedIn(),
-	(req, res) => {
-		const courseId = parseInt(req.query['courseId'] as string, 10) - 1;
-		const lessonId = parseInt(req.query['lessonId'] as string, 10) - 1;
-		if (isNaN(courseId)) {
-			zeykerokyu.getReviewableItems(req.user!, (items: LearnableItem[]) => {
-				res.json(items);
-			});
-		} else if (isNaN(lessonId)) {
-			zeykerokyu.getReviewableItemsForCourse(courseId, req.user!, (items: LearnableItem[]) => {
-				res.json(items);
-			});
-		} else {
-			zeykerokyu.getReviewableItemsForLesson(courseId, lessonId, req.user!, (items: LearnableItem[]) => {
-				res.json(items);
-			});
+	async (req, res, next) => {
+		try {
+			const courseId = parseInt(req.query['courseId'] as string, 10) - 1;
+			const lessonId = parseInt(req.query['lessonId'] as string, 10) - 1;
+			let items: LearnableItem[];
+			if (isNaN(courseId)) {
+				items = await zeykerokyu.getReviewableItems(req.user!);
+			} else if (isNaN(lessonId)) {
+				items = await zeykerokyu.getReviewableItemsForCourse(courseId, req.user!);
+			} else {
+				items = await zeykerokyu.getReviewableItemsForLesson(courseId, lessonId, req.user!);
+			}
+			res.json(items);
+		} catch (e) {
+			next(e);
 		}
 	}
 );
 
 router.get('/srs/reviewable-count',
 	checkLoggedIn(),
-	(req, res) => {
-		const courseId = parseInt(req.query['courseId'] as string, 10) - 1;
-		const lessonId = parseInt(req.query['lessonId'] as string, 10) - 1;
-		if (isNaN(courseId)) {
-			zeykerokyu.getReviewableCount(req.user!, (count: number) => {
-				res.json(count);
-			});
-		} else if (isNaN(lessonId)) {
-			zeykerokyu.getReviewableCountForCourse(courseId, req.user!, (count: number) => {
-				res.json(count);
-			});
-		} else {
-			zeykerokyu.getReviewableCountForLesson(courseId, lessonId, req.user!, (count: number) => {
-				res.json(count);
-			});
+	async (req, res, next) => {
+		try {
+			const courseId = parseInt(req.query['courseId'] as string, 10) - 1;
+			const lessonId = parseInt(req.query['lessonId'] as string, 10) - 1;
+			if (isNaN(courseId)) {
+				res.json(await zeykerokyu.getReviewableCount(req.user!));
+			} else if (isNaN(lessonId)) {
+				res.json(await zeykerokyu.getReviewableCountForCourse(courseId, req.user!));
+			} else {
+				res.json(await zeykerokyu.getReviewableCountForLesson(courseId, lessonId, req.user!));
+			}
+		} catch (e) {
+			next(e);
 		}
 	}
 );
@@ -326,30 +339,39 @@ router.get('/srs/reviewable-count',
 router.post('/srs/mark-correct',
 	checkLoggedIn(),
 	parseIntegerParameter('vocab', 'post'),
-	(req, res) => {
-		zeykerokyu.processCorrectAnswer(req.user!, req.args!['vocab'], (items) => {
+	async (req, res, next) => {
+		try {
+			await zeykerokyu.processCorrectAnswer(req.user!, req.args!['vocab']);
 			res.status(204).send();
-		});
+		} catch (e) {
+			next(e);
+		}
 	}
 );
 
 router.post('/srs/mark-incorrect',
 	checkLoggedIn(),
 	parseIntegerParameter('vocab', 'post'),
-	(req, res) => {
-		zeykerokyu.processIncorrectAnswer(req.user!, req.args!['vocab'], () => {
+	async (req, res, next) => {
+		try {
+			await zeykerokyu.processIncorrectAnswer(req.user!, req.args!['vocab']);
 			res.status(204).send();
-		});
+		} catch (e) {
+			next(e);
+		}
 	}
 );
 
 router.post('/srs/mark-known',
 	checkLoggedIn(),
 	parseIntegerParameter('vocab', 'post'),
-	(req, res) => {
-		zeykerokyu.processKnownAnswer(req.user!, req.args!['vocab'], () => {
+	async (req, res, next) => {
+		try {
+			await zeykerokyu.processKnownAnswer(req.user!, req.args!['vocab']);
 			res.status(204).send();
-		});
+		} catch (e) {
+			next(e);
+		}
 	}
 );
 

src/types/utility-types.d.ts
@@ -1,1 +1,0 @@
-type Callback<T> = (data: T) => void;

src/zeykerokyu.ts
@@ -18,93 +18,98 @@ import db from './db';
 import * as reykunyu from './reykunyu';
 
 /// Returns (in a callback) a list of available courses.
-export function getCourses(cb: (data: Course[]) => void): void {
-	db.all(`select c.id, c.name, c.description,
-		(select count() from lesson l where l.course_id == c.id) as lesson_count
-	from course c`, (err, courses: Course[]) => {
-		if (err) {
-			console.log(err);
-			throw err;
-		}
-		cb(courses);
-	});
-}
-
-export function getCourseData(courseId: number, cb: (data: Course) => void): void {
-	db.get(`select id, name, description
-	from course
-	where id == ?`, courseId, (err, course: Course) => {
-		if (err) {
-			console.log(err);
-			throw err;
-		}
-		cb(course);
-	});
-}
-
-/// Returns (in a callback) a list of available lessons in a course, with
-/// statistics for the given user.
-export function getLessons(user: Express.User, courseId: number, cb: Callback<Lesson[]>): void {
-	if (user) {
-		db.all(`select l.id, l.name, l.introduction, l.conclusion,
-			(select count() from vocab_in_lesson v
-				where l.course_id == v.course_id and l.id == v.lesson_id) as total_count,
-			(select count() from vocab_in_lesson v, vocab_status s
-				where l.course_id == v.course_id and l.id == v.lesson_id
-				and v.vocab == s.vocab and s.user == ?1) as known_count,
-			(select count() from vocab_in_lesson v, vocab_status s
-				where l.course_id == v.course_id and l.id == v.lesson_id
-				and v.vocab == s.vocab and s.user == ?1 and s.next_review <= current_timestamp) as reviewable_count
-		from lesson l
-		where l.course_id = ?2`, user.username, courseId, (err: Error | null, lessons: Lesson[]) => {
+export async function getCourses(): Promise<Course[]> {
+	return new Promise((fulfill, reject) => {
+		db.all(`select c.id, c.name, c.description,
+			(select count() from lesson l where l.course_id == c.id) as lesson_count
+		from course c`, (err, courses: Course[]) => {
 			if (err) {
-				console.log(err);
-				throw err;
+				reject(err);
 			}
-			cb(lessons);
+			fulfill(courses);
+		});
+	});
+}
+
+export async function getCourseData(courseId: number): Promise<Course> {
+	return new Promise((fulfill, reject) => {
+		db.get(`select id, name, description
+		from course
+		where id == ?`, courseId, (err, course: Course) => {
+			if (err) {
+				reject(err);
+			}
+			fulfill(course);
+		});
+	});
+}
+
+/// Returns a list of available lessons in a course, with statistics for the
+/// given user.
+export async function getLessons(user: Express.User, courseId: number): Promise<Lesson[]> {
+	if (user) {
+		return new Promise((fulfill, reject) => {
+			db.all(`select l.id, l.name, l.introduction, l.conclusion,
+				(select count() from vocab_in_lesson v
+					where l.course_id == v.course_id and l.id == v.lesson_id) as total_count,
+				(select count() from vocab_in_lesson v, vocab_status s
+					where l.course_id == v.course_id and l.id == v.lesson_id
+					and v.vocab == s.vocab and s.user == ?1) as known_count,
+				(select count() from vocab_in_lesson v, vocab_status s
+					where l.course_id == v.course_id and l.id == v.lesson_id
+					and v.vocab == s.vocab and s.user == ?1 and s.next_review <= current_timestamp) as reviewable_count
+			from lesson l
+			where l.course_id = ?2`, user.username, courseId, (err: Error | null, lessons: Lesson[]) => {
+				if (err) {
+					reject(err);
+				}
+				fulfill(lessons);
+			});
 		});
 
 	} else {
-		db.all(`select l.id, l.name, l.description,
-			(select count() from vocab_in_lesson v where l.id == v.lesson_id) as total_count,
-		from lesson l
-		where l.course_id = ?`, courseId, (err, lessons: Lesson[]) => {
-			if (err) {
-				console.log(err);
-				throw err;
-			}
-			cb(lessons);
+		return new Promise((fulfill, reject) => {
+			db.all(`select l.id, l.name, l.description,
+				(select count() from vocab_in_lesson v where l.id == v.lesson_id) as total_count,
+			from lesson l
+			where l.course_id = ?`, courseId, (err, lessons: Lesson[]) => {
+				if (err) {
+					reject(err);
+				}
+				fulfill(lessons);
+			});
 		});
 	}
 }
 
-export function getLessonData(courseId: number, lessonId: number, cb: Callback<Lesson>): void {
-	db.get(`select course_id, id, name, introduction, conclusion
-	from lesson
-	where course_id == ? and id == ?`, courseId, lessonId, (err: Error | null, lesson: Lesson) => {
-		if (err) {
-			console.log(err);
-			throw err;
-		}
-		cb(lesson);
+export async function getLessonData(courseId: number, lessonId: number): Promise<Lesson> {
+	return new Promise((fulfill, reject) => {
+		db.get(`select course_id, id, name, introduction, conclusion
+		from lesson
+		where course_id == ? and id == ?`, courseId, lessonId, (err: Error | null, lesson: Lesson) => {
+			if (err) {
+				reject(err);
+			}
+			fulfill(lesson);
+		});
 	});
 }
 
 /** LearnableItem as it comes out of the database, with a word ID instead of WordData. */
 type UnprocessedLearnableItem = { 'vocab': number, 'comment'?: string };
 
-export function getItemsForLesson(courseId: number, lessonId: number, user: Express.User, cb: Callback<LearnableItem[]>): void {
-	db.all(`select v.vocab, v.comment from vocab_in_lesson v
-		where v.course_id == ? and v.lesson_id == ?
-		`, courseId, lessonId, (err: Error | null, items: UnprocessedLearnableItem[]) => {
-			if (err) {
-				console.log(err);
-				throw err;
+export async function getItemsForLesson(courseId: number, lessonId: number, user: Express.User): Promise<LearnableItem[]> {
+	return new Promise((fulfill, reject) => {
+		db.all(`select v.vocab, v.comment from vocab_in_lesson v
+			where v.course_id == ? and v.lesson_id == ?
+			`, courseId, lessonId, (err: Error | null, items: UnprocessedLearnableItem[]) => {
+				if (err) {
+					reject(err);
+				}
+				fulfill(vocabIDsToWordData(items));
 			}
-			let result = vocabIDsToWordData(items);
-			cb(result);
-		}
-	);
+		);
+	});
 }
 
 function vocabIDsToWordData(items: UnprocessedLearnableItem[]): LearnableItem[] {
@@ -121,129 +126,132 @@ function vocabIDsToWordData(items: UnprocessedLearnableItem[]): LearnableItem[] 
 	return result;
 }
 
-export function getLearnableItemsForLesson(courseId: number, lessonId: number, user: Express.User, cb: Callback<LearnableItem[]>): void {
-	db.all(`select v.vocab, v.comment from vocab_in_lesson v
-		where v.course_id == ? and v.lesson_id == ?
-			and v.vocab not in (
-				select vocab
-				from vocab_status
-				where user == ?
-			)
-		`, courseId, lessonId, user.username, (err: Error | null, items: UnprocessedLearnableItem[]) => {
-			if (err) {
-				console.log(err);
-				throw err;
+export async function getLearnableItemsForLesson(courseId: number, lessonId: number, user: Express.User): Promise<LearnableItem[]> {
+	return new Promise((fulfill, reject) => {
+		db.all(`select v.vocab, v.comment from vocab_in_lesson v
+			where v.course_id == ? and v.lesson_id == ?
+				and v.vocab not in (
+					select vocab
+					from vocab_status
+					where user == ?
+				)
+			`, courseId, lessonId, user.username, (err: Error | null, items: UnprocessedLearnableItem[]) => {
+				if (err) {
+					reject(err);
+				}
+				fulfill(vocabIDsToWordData(items));
 			}
-			let result = vocabIDsToWordData(items);
-			cb(result);
-		}
-	);
+		);
+	});
 }
 
-export function getReviewableItems(user: Express.User, cb: Callback<LearnableItem[]>): void {
-	db.all(`select distinct v.vocab
-		from vocab_status s, vocab_in_lesson v
-		where s.user == ?
-			and s.next_review <= current_timestamp
-			and v.vocab == s.vocab
-		order by random()
-		`, user.username, (err: Error | null, items: UnprocessedLearnableItem[]) => {
-			if (err) {
-				console.log(err);
-				throw err;
+export async function getReviewableItems(user: Express.User): Promise<LearnableItem[]> {
+	return new Promise((fulfill, reject) => {
+		db.all(`select distinct v.vocab
+			from vocab_status s, vocab_in_lesson v
+			where s.user == ?
+				and s.next_review <= current_timestamp
+				and v.vocab == s.vocab
+			order by random()
+			`, user.username, (err: Error | null, items: UnprocessedLearnableItem[]) => {
+				if (err) {
+					reject(err);
+				}
+				fulfill(vocabIDsToWordData(items));
 			}
-			let result = vocabIDsToWordData(items);
-			cb(result);
-		}
-	);
+		);
+	});
 }
 
-export function getReviewableCount(user: Express.User, cb: Callback<number>): void {
-	db.get(`select count(distinct v.vocab)
-		from vocab_status s, vocab_in_lesson v
-		where s.user == ?
-			and s.next_review <= current_timestamp
-			and v.vocab == s.vocab
-		`, user.username, (err, result: Record<'count(distinct v.vocab)', number>) => {
-			if (err) {
-				console.log(err);
-				throw err;
+export async function getReviewableCount(user: Express.User): Promise<number> {
+	return new Promise((fulfill, reject) => {
+		db.get(`select count(distinct v.vocab)
+			from vocab_status s, vocab_in_lesson v
+			where s.user == ?
+				and s.next_review <= current_timestamp
+				and v.vocab == s.vocab
+			`, user.username, (err, result: Record<'count(distinct v.vocab)', number>) => {
+				if (err) {
+					reject(err);
+				}
+				fulfill(result['count(distinct v.vocab)']);
 			}
-			cb(result['count(distinct v.vocab)']);
-		}
-	);
+		);
+	});
 }
 
-export function getReviewableItemsForCourse(courseId: number, user: Express.User, cb: Callback<LearnableItem[]>): void {
-	db.all(`select distinct v.vocab
-		from vocab_status s, vocab_in_lesson v
-		where s.user == ?
-			and s.next_review <= current_timestamp
-			and v.vocab == s.vocab
-			and v.course_id == ?
-		order by random()
-		`, user.username, courseId, (err: Error | null, items: UnprocessedLearnableItem[]) => {
-			if (err) {
-				console.log(err);
-				throw err;
+export async function getReviewableItemsForCourse(courseId: number, user: Express.User): Promise<LearnableItem[]> {
+	return new Promise((fulfill, reject) => {
+		db.all(`select distinct v.vocab
+			from vocab_status s, vocab_in_lesson v
+			where s.user == ?
+				and s.next_review <= current_timestamp
+				and v.vocab == s.vocab
+				and v.course_id == ?
+			order by random()
+			`, user.username, courseId, (err: Error | null, items: UnprocessedLearnableItem[]) => {
+				if (err) {
+					reject(err);
+				}
+				fulfill(vocabIDsToWordData(items));
 			}
-			let result = vocabIDsToWordData(items);
-			cb(result);
-		}
-	);
+		);
+	});
 }
 
-export function getReviewableCountForCourse(courseId: number, user: Express.User, cb: Callback<number>): void {
-	db.get(`select count(distinct v.vocab)
-		from vocab_status s, vocab_in_lesson v
-		where s.user == ?
-			and s.next_review <= current_timestamp
-			and v.vocab == s.vocab
-			and v.course_id == ?
-		`, user.username, courseId, (err: Error | null, result: Record<'count(distinct v.vocab)', number>) => {
-			if (err) {
-				console.log(err);
-				throw err;
+export async function getReviewableCountForCourse(courseId: number, user: Express.User): Promise<number> {
+	return new Promise((fulfill, reject) => {
+		db.get(`select count(distinct v.vocab)
+			from vocab_status s, vocab_in_lesson v
+			where s.user == ?
+				and s.next_review <= current_timestamp
+				and v.vocab == s.vocab
+				and v.course_id == ?
+			`, user.username, courseId, (err: Error | null, result: Record<'count(distinct v.vocab)', number>) => {
+				if (err) {
+					reject(err);
+				}
+				fulfill(result['count(distinct v.vocab)']);
 			}
-			cb(result['count(distinct v.vocab)']);
-		}
-	);
+		);
+	});
 }
 
-export function getReviewableItemsForLesson(courseId: number, lessonId: number, user: Express.User, cb: Callback<LearnableItem[]>): void {
-	db.all(`select distinct v.vocab
-		from vocab_status s, vocab_in_lesson v
-		where s.user == ?
-			and s.next_review <= current_timestamp
-			and v.vocab == s.vocab
-			and v.course_id == ? and v.lesson_id == ?
-		order by random()
-		`, user.username, courseId, lessonId, (err: Error | null, items: UnprocessedLearnableItem[]) => {
-			if (err) {
-				console.log(err);
-				throw err;
+export async function getReviewableItemsForLesson(courseId: number, lessonId: number, user: Express.User): Promise<LearnableItem[]> {
+	return new Promise((fulfill, reject) => {
+		db.all(`select distinct v.vocab
+			from vocab_status s, vocab_in_lesson v
+			where s.user == ?
+				and s.next_review <= current_timestamp
+				and v.vocab == s.vocab
+				and v.course_id == ? and v.lesson_id == ?
+			order by random()
+			`, user.username, courseId, lessonId, (err: Error | null, items: UnprocessedLearnableItem[]) => {
+				if (err) {
+					reject(err);
+				}
+				fulfill(vocabIDsToWordData(items));
 			}
-			let result = vocabIDsToWordData(items);
-			cb(result);
-		}
-	);
+		);
+	});
 }
 
-export function getReviewableCountForLesson(courseId: number, lessonId: number, user: Express.User, cb: Callback<number>): void {
-	db.get(`select count(distinct v.vocab)
-		from vocab_status s, vocab_in_lesson v
-		where s.user == ?
-			and s.next_review <= current_timestamp
-			and v.vocab == s.vocab
-			and v.course_id == ? and v.lesson_id == ?
-		`, user.username, courseId, lessonId, (err: Error | null, result: Record<'count(distinct v.vocab)', number>) => {
-			if (err) {
-				console.log(err);
-				throw err;
+export async function getReviewableCountForLesson(courseId: number, lessonId: number, user: Express.User): Promise<number> {
+	return new Promise((fulfill, reject) => {
+		db.get(`select count(distinct v.vocab)
+			from vocab_status s, vocab_in_lesson v
+			where s.user == ?
+				and s.next_review <= current_timestamp
+				and v.vocab == s.vocab
+				and v.course_id == ? and v.lesson_id == ?
+			`, user.username, courseId, lessonId, (err: Error | null, result: Record<'count(distinct v.vocab)', number>) => {
+				if (err) {
+					reject(err);
+				}
+				fulfill(result['count(distinct v.vocab)']);
 			}
-			cb(result['count(distinct v.vocab)']);
-		}
-	);
+		);
+	});
 }
 
 /// If an item is put into stage i, then its next review should be scheduled
@@ -267,101 +275,101 @@ const intervalDuration = [
 /// item. This can also be called for items that have not been learned yet; in
 /// this case they will move from stage 0 (which is not in the database) to 1
 /// and scheduled for immediate review.
-export function processCorrectAnswer(user: Express.User, vocab: number, cb: Callback<void>): void {
-	// get current SRS stage
-	db.get(`select srs_stage from vocab_status
-		where user == ?
-			and vocab == ?`,
-		user.username, vocab, (err: any, result: Record<'srs_stage', number>) => {
-			if (err) {
-				console.log(err);
-				throw err;
-			} else if (!result) {
-				// if not in database (= stage 0), insert it
-				db.run(`insert into vocab_status
-					values (?, ?, ?, current_timestamp)`,
-					user.username, vocab, 1, (err: any) => {
-						if (err) {
-							console.log(err);
-							throw err;
-						} else {
-							cb();
+export async function processCorrectAnswer(user: Express.User, vocab: number): Promise<void> {
+	return new Promise((fulfill, reject) => {
+		// get current SRS stage
+		db.get(`select srs_stage from vocab_status
+			where user == ?
+				and vocab == ?`,
+			user.username, vocab, (err: any, result: Record<'srs_stage', number>) => {
+				if (err) {
+					reject(err);
+				} else if (!result) {
+					// if not in database (= stage 0), insert it
+					db.run(`insert into vocab_status
+						values (?, ?, ?, current_timestamp)`,
+						user.username, vocab, 1, (err: any) => {
+							if (err) {
+								reject(err);
+							} else {
+								fulfill();
+							}
 						}
-					}
-				);
-			} else {
-				// otherwise update the SRS stage and next review time in the
-				// database
-				const stage = result['srs_stage'];
-				const newStage = Math.min(stage + 1, intervalDuration.length - 1);
-				db.run(`update vocab_status
-					set (srs_stage, next_review) = (?, datetime(current_timestamp, "+" || ?))
-					where user == ?
-						and vocab == ?`,
-					newStage, intervalDuration[newStage], user.username, vocab, (err: any) => {
-						if (err) {
-							console.log(err);
-							throw err;
-						} else {
-							cb();
+					);
+				} else {
+					// otherwise update the SRS stage and next review time in the
+					// database
+					const stage = result['srs_stage'];
+					const newStage = Math.min(stage + 1, intervalDuration.length - 1);
+					db.run(`update vocab_status
+						set (srs_stage, next_review) = (?, datetime(current_timestamp, "+" || ?))
+						where user == ?
+							and vocab == ?`,
+						newStage, intervalDuration[newStage], user.username, vocab, (err: any) => {
+							if (err) {
+								reject(err);
+							} else {
+								fulfill();
+							}
 						}
-					}
-				);
+					);
+				}
 			}
-		}
-	);
+		);
+	});
 }
 
 /// Decreases the SRS stage for an item and schedules the next review for the
 /// item. Can be called only for vocab whose status is already stored in the
 /// database, i.e., not for newly learned items.
-export function processIncorrectAnswer(user: Express.User, vocab: number, cb: Callback<void>): void {
-	// get current SRS stage
-	db.get(`select srs_stage from vocab_status
-		where user == ?
-			and vocab == ?`,
-		user.username, vocab, (err: any, result: Record<'srs_stage', number>) => {
-			if (err) {
-				console.log(err);
-				throw err;
-			} else if (!result) {
-				throw Error('Tried to decrease SRS level for [' + vocab + '] which is not in the database');
-			} else {
-				// otherwise update the SRS stage and next review time in the
-				// database
-				let stage = result['srs_stage'];
-				const newStage = 1;
-				db.run(`update vocab_status
-					set (srs_stage, next_review) = (?, datetime(current_timestamp, "+" || ?))
-					where user == ?
-						and vocab == ?`,
-					newStage, intervalDuration[newStage], user.username, vocab, (err: any) => {
-						if (err) {
-							console.log(err);
-							throw err;
-						} else {
-							cb();
+export async function processIncorrectAnswer(user: Express.User, vocab: number): Promise<void> {
+	return new Promise((fulfill, reject) => {
+		// get current SRS stage
+		db.get(`select srs_stage from vocab_status
+			where user == ?
+				and vocab == ?`,
+			user.username, vocab, (err: any, result: Record<'srs_stage', number>) => {
+				if (err) {
+					reject(err);
+				} else if (!result) {
+					reject('Tried to decrease SRS level for [' + vocab + '] which is not in the database');
+				} else {
+					// otherwise update the SRS stage and next review time in the
+					// database
+					let stage = result['srs_stage'];
+					const newStage = 1;
+					db.run(`update vocab_status
+						set (srs_stage, next_review) = (?, datetime(current_timestamp, "+" || ?))
+						where user == ?
+							and vocab == ?`,
+						newStage, intervalDuration[newStage], user.username, vocab, (err: any) => {
+							if (err) {
+								reject(err);
+							} else {
+								fulfill();
+							}
 						}
-					}
-				);
+					);
+				}
 			}
-		}
-	);
+		);
+	});
 }
 
 /// Increases the SRS stage for an item to the maximum level, *and* schedules it
 /// for immediate review. Can be called only for items that are not yet in the
 /// database.
-export function processKnownAnswer(user: Express.User, vocab: number, cb: Callback<void>): void {
-	db.run(`insert into vocab_status
-		values (?, ?, ?, current_timestamp)`,
-		user.username, vocab, intervalDuration.length - 1, (err: any) => {
-			if (err) {
-				console.log(err);
-				throw err;
-			} else {
-				cb();
+export async function processKnownAnswer(user: Express.User, vocab: number): Promise<void> {
+	return new Promise((fulfill, reject) => {
+		db.run(`insert into vocab_status
+			values (?, ?, ?, current_timestamp)`,
+			user.username, vocab, intervalDuration.length - 1, (err: any) => {
+				if (err) {
+					reject(err);
+				} else {
+					fulfill();
+				}
 			}
-		}
-	);
+		);
+	});
 }

commit 0997a8b906a129a1e1f15f780bd7c22d8563f7d9

Fix learning sessions
This was broken since commit d3565565 due to one missing async. It is a
bit sad that TypeScript doesn't catch this error due to JSON responses
not being typed. Maybe something to consider in the future.

Fixes #224.

src/api.ts
@@ -288,7 +288,7 @@ router.get('/srs/learnable',
 	parseIntegerParameter('lessonId', 'get'),
 	async (req, res, next) => {
 		try {
-			const items = zeykerokyu.getLearnableItemsForLesson(req.args!['courseId'] - 1, req.args!['lessonId'] - 1, req.user!);
+			const items = await zeykerokyu.getLearnableItemsForLesson(req.args!['courseId'] - 1, req.args!['lessonId'] - 1, req.user!);
 			res.json(items);
 		} catch (e) {
 			next(e);