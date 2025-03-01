5b7d52820e481f3f1c1f4c4477144b57e4476af9
Willem Sonke <willem@wimiso.nl>
2025-03-01 14:12:32 +0100

    Rewrite the rhyme checker
    This approach is simpler and it fixes bugs with diphthongs. Also added
    tests.
    
    Fixes #225.

--- src/rhymes.ts
+++ src/rhymes.ts
@@ -2,39 +2,27 @@
  * Functions to determine if two Na'vi words rhyme.
  */
 
-import * as convert from "./convert";
 import * as phonology from "./phonology";
 
+export function rhymeEnding(word: string): string {
+	if (word.length === 0) {
+		return '';
+	}
+
+	// ignore é and ù
+	word = word.replace('é', 'e');
+	word = word.replace('ù', 'u');
+
+	let ending = '';
+	while (word.length > 0 && !phonology.endsInVowel(word)) {
+		ending = word[word.length - 1] + ending;
+		word = word.substring(0, word.length - 1);
+	}
+	ending = word[word.length - 1] + ending;
+	return ending;
+}
+
 export function rhymes(first: string, second: string): boolean {
-	if (first === second) {
-		return false;
-	}
-
-	first = convert.compress(first).replace('u', 'ù');
-	second = convert.compress(second).replace('u', 'ù');
-
-	// find last vowel of first
-	const n = first.length;
-	let lastVowel = null;
-	for (let i = 0; i < first.length; i++) {
-		if (phonology.isVowel(first[n - i - 1]) ||
-				phonology.isDiphthong(first[n - i - 1])) {
-			lastVowel = i;
-			break;
-		}
-	}
-	if (lastVowel === null) {
-		return false;
-	}
-
-	// then check if first and second are identical until the last vowel
-	const m = second.length;
-	for (let i = 0; i <= lastVowel; i++) {
-		if (first[n - i - 1] !== second[m - i - 1]) {
-			return false;
-		}
-	}
-
-	return true;
+	return rhymeEnding(first) === rhymeEnding(second);
 }
 
---
+++ test/rhymes.ts
@@ -0,0 +1,43 @@
+import { describe, test } from 'node:test';
+import assert from 'node:assert';
+
+import * as rhymes from '../src/rhymes';
+
+describe('rhymes', () => {
+
+	describe('the rhyme ending', () => {
+		test('includes only the last vowel, if the noun ends in a vowel', (t) => {
+			assert.strictEqual(rhymes.rhymeEnding('a'), 'a');
+			assert.strictEqual(rhymes.rhymeEnding('tute'), 'e');
+			assert.strictEqual(rhymes.rhymeEnding('kaltxì'), 'ì');
+			assert.strictEqual(rhymes.rhymeEnding('meoauniaea'), 'a');
+		});
+
+		test('includes only the last diphthong, if the noun ends in a diphthong', (t) => {
+			assert.strictEqual(rhymes.rhymeEnding('pxaw'), 'aw');
+			assert.strictEqual(rhymes.rhymeEnding('hayalovay'), 'ay');
+		});
+
+		test('is e even for nouns ending with é', (t) => {
+			assert.strictEqual(rhymes.rhymeEnding('tuté'), 'e');
+		});
+
+		test('is u even for nouns ending with ù', (t) => {
+			assert.strictEqual(rhymes.rhymeEnding('kaltxù'), 'u');
+		});
+
+		test('includes the last vowel and consonant, if the noun ends in a vowel and a consonant', (t) => {
+			assert.strictEqual(rhymes.rhymeEnding('om'), 'om');
+			assert.strictEqual(rhymes.rhymeEnding('\'o\''), 'o\'');
+			assert.strictEqual(rhymes.rhymeEnding('lor'), 'or');
+			assert.strictEqual(rhymes.rhymeEnding('sngap'), 'ap');
+			assert.strictEqual(rhymes.rhymeEnding('kxitx'), 'itx');
+		});
+
+		test('includes the last diphthong and consonant, if the noun ends in a diphthong and a consonant', (t) => {
+			assert.strictEqual(rhymes.rhymeEnding('teyr'), 'eyr');
+			assert.strictEqual(rhymes.rhymeEnding('lawk'), 'awk');
+			assert.strictEqual(rhymes.rhymeEnding('\'awkx'), 'awkx');
+		});
+	});
+});
