b00f289ace3265549e69aef1e7c2b39a50d0b0a9
lauterb <47546076+lauterb@users.noreply.github.com>
2025-04-29 21:08:50 -0400

    remove old commented out test code

--- src/edit.ts
+++ src/edit.ts
@@ -94,8 +94,6 @@ export function insertWordData(newData: WordData, user: Express.User): void {
 		'data': newData
 	});
 	fs.writeFileSync("./data/history.json", JSON.stringify(history));
-	//console.log("debug test deleteWordData, with ",id);
-	//deleteWordData(id,user);
 }
 
 export function getAll(): WordData[] {
