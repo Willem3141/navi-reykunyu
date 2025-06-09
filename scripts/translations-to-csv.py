d018e0de4d9988ee60f3df31194243aeca0794b4
Willem Sonke <willem@wimiso.nl>
2025-06-09 19:26:09 +0200

    Add scripts to enable translations in CSV

---
+++ scripts/translations-to-csv.py
@@ -0,0 +1,18 @@
+import csv
+import json
+
+languages = ['en', 'de', 'nl', 'fr', 'da', 'hu', 'x-navi', 'et', 'pl', 'ru', 'sv']
+
+with open('src/translations.json') as translationsFile:
+	translations = json.load(translationsFile)
+
+with open('src/translations.csv', 'w') as outputFile:
+	writer = csv.writer(outputFile)
+	for key in translations['en'].keys():
+		row = [key]
+		for lang in languages:
+			if key in translations[lang]:
+				row.append(translations[lang][key])
+			else:
+				row.append('')
+		writer.writerow(row)
