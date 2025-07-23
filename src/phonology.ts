d30b7e845e0b13e4a789de4952ead6d47bf9e642
Willem Sonke <willem@wimiso.nl>
2025-07-23 20:32:18 +0200

    Rewrite the noun conjugator to be more modular
    Instead of handling all types of pre- and suffixes separately, we now
    apply them one by one in a more generic manner.
    
    Right now the simplified conjugation view (that is visible in
    conjugation tables) doesn't work correctly. The
    forbidden-by-external-lenition check is also broken.

--- src/phonology.ts
+++ src/phonology.ts
@@ -72,13 +72,9 @@ const lenitions: Record<string, string> = {
 };
 
 /**
- * Returns the lenited form of the given word. The return value is an array
- * containing first the actually lenited first consonant, and second the
- * (unchanged) rest of the word. If the given word is not lenitable, the first
- * element of the return value is an empty string, while the second element is
- * simply the entire word.
+ * Returns the lenited form of the given word.
  */
-export function lenite(word: string): [string, string] {
+export function lenite(word: string): string {
 	let lenitedConsonant = '';
 	let rest = word;
 
@@ -97,5 +93,5 @@ export function lenite(word: string): [string, string] {
 		lenitedConsonant = lenitedConsonant.toUpperCase();
 	}
 
-	return [lenitedConsonant, rest];
+	return lenitedConsonant + rest;
 }
