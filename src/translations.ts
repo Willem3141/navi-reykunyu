42c8471ba5a6094709ea2bfb7825746b6bc77901
Willem Sonke <willem@wimiso.nl>
2026-02-13 02:00:05 +0100

    Implement a parser for plurality patterns

--- src/translations.ts
+++ src/translations.ts
@@ -3,14 +3,14 @@
  * but it also needs to have access to translations in the client-side JS,
  * because parts of the UI are rendered client-side. This also allows language
  * changes to be handled without a page reload.
- * 
+ *
  * The translations are loaded from a simple JSON file, `src/translations.json`,
  * which for each language contains a set of keys and strings.
- * 
+ *
  * On the server-side, use the function `_` to get a translation, or `span_`
  * when inserting something in HTML that needs to be re-rendered on the
  * client-side when the user changes the language.
- * 
+ *
  * On the client-side, there also is a function `_` available, which is used the
  * same way. This function is provided by `ui-translations.js`. The server
  * pastes the translation JSON into this JS file, so that the exact same
@@ -29,7 +29,147 @@ try {
 	output.error('translations.json not found, exiting');
 	process.exit(1);
 }
-const strings: { [language: string]: { [key: string]: string } } = JSON.parse(stringsJSON);
+type Strings = { [language: string]: { [key: string]: string } };
+const strings: Strings = JSON.parse(stringsJSON);
+
+/**
+ * A plural pattern is a function that maps an input number (which is to be
+ * inserted into a translated string) to the index of its plurality form. For
+ * example, for English `n == 1` results in 0 (= singular), while any other `n`
+ * results in 1 (= plural). Other languages may have different behavior.
+ */
+export class PluralPattern {
+
+	private checkers: (string|number)[][] = [];
+
+	/**
+	 * Generates a plural pattern based on a string representation such as
+	 * "n == 1; n == 2; n == 3".
+	 */
+	constructor(pattern: string) {
+		const forms = pattern.split(';').map((s: string) => s.trim());
+		for (let form of forms) {
+			this.checkers.push(this.parseForm(form));
+		}
+	}
+
+	private lexerRegex = / |\d+|n|&&|\|\||==|!=|<=|>=|[()<>%]/g;
+
+	/**
+	 * Lexes the input string into tokens.
+	 */
+	private *tokenize(form: string): Generator<string> {
+		let matches = form.matchAll(this.lexerRegex);
+		if (!matches) {
+			return;
+		}
+		let i = 0;
+		for (let match of matches) {
+			if (match.index !== i) {
+				throw Error('invalid plural pattern ' + form + ' (position ' + i + ')');
+			}
+			i += match[0].length;
+			if (match[0] !== ' ') {
+				yield match[0];
+			}
+		}
+	}
+
+	private operatorPrecedence: Record<string, number> = {
+		'%': 2,
+		'==': 1, '!=': 1, '<': 1, '>': 1, '<=': 1, '>=': 1,
+		'&&': 0, '||': 0
+	};
+
+	private parseForm(form: string): (string|number)[] {
+		// Use a simple shunting yard parser to turn the string into RPN.
+		let spur: (string|number)[] = ['('];
+		let output: (string | number)[] = [];
+
+		for (let token of this.tokenize(form)) {
+			let integer: number;
+			if (token === 'n') {
+				output.push('n');
+			} else if (!Number.isNaN(integer = parseInt(token, 10))) {
+				output.push(integer);
+			} else if (token === '(') {
+				spur.push('(');
+			} else if (token === ')') {
+				while (spur[spur.length - 1] !== '(') {
+					output.push(spur.pop()!);
+				}
+				spur.pop();
+			} else {  // token is an operator
+				while (spur[spur.length - 1] !== '(' &&
+						this.operatorPrecedence[token] <= this.operatorPrecedence[spur[spur.length - 1]]) {
+					output.push(spur.pop()!);
+				}
+				spur.push(token);
+			}
+		}
+
+		while (spur.length > 1) {  // leave the sentinel '('
+			output.push(spur.pop()!);
+		}
+
+		return output;
+	}
+
+	checkerFor(i: number): (string | number)[] {
+		return this.checkers[i];
+	}
+
+	evaluate(n: number): number {
+		for (let i = 0; i < this.checkers.length; i++) {
+			if (this.evaluateStackCode(n, this.checkers[i])) {
+				return i;
+			}
+		}
+		return this.checkers.length;
+	}
+
+	private evaluateStackCode(n: number, code: (string|number)[]): boolean {
+		let stack: number[] = [];
+		for (let i = 0; i < code.length; i++) {
+			let op = code[i];
+			if (op === 'n') {
+				stack.push(n);
+			} else if (typeof op === 'number') {
+				stack.push(op);
+			} else {
+				let i2 = stack.pop()!;
+				let i1 = stack.pop()!;
+				if (op === '%') {
+					stack.push(i1 % i2);
+				} else {
+					let result =
+						op === '==' ? i1 === i2 :
+						op === '!=' ? i1 !== i2 :
+						op === '<' ? i1 < i2 :
+						op === '>' ? i1 > i2 :
+						op === '<=' ? i1 <= i2 :
+						op === '>=' ? i1 >= i2 :
+						op === '&&' ? i1 && i2 : i1 || i2;
+					stack.push(result ? 1 : 0);
+				}
+			}
+		}
+		return stack.pop() != 0;
+	}
+}
+
+let pluralPatterns: { [language: string]: PluralPattern } = {};
+for (let language in strings) {
+	try {
+		pluralPatterns[language] = new PluralPattern(strings[language]['plurality-pattern']);
+	} catch (e) {
+		output.warning('Invalid plurality pattern \'' + strings[language]['plurality-pattern'] + '\' for language ' + language);
+		output.hint('Reykunyu expects, for each UI language, a value \'plurality-pattern\'\n' +
+			'that specifies the plural forms the language has. This value is either\n' +
+			'not present or syntactically invalid.',
+			'invalid-plurality-pattern');
+	}
+}
 
 /** The active language. */
 let lang = 'en';
@@ -45,19 +185,30 @@ export function getLanguage(): string {
 }
 
 /** Returns the translation for the active language for the given key. */
-export function _(key: string): string {
+export function _(key: string, ...parameters: number[]): string {
 	if (strings.hasOwnProperty(lang) && strings[lang].hasOwnProperty(key)) {
-		return strings[lang][key];
+		return fillInParameters(strings[lang][key], lang, parameters);
 	} else if (strings['en'].hasOwnProperty(key)) {
-		return strings['en'][key];
+		return fillInParameters(strings['en'][key], 'en', parameters);
 	} else {
 		return '[' + key + ']';
 	}
 }
 
+function fillInParameters(text: string, language: string, parameters: number[]): string {
+	let result = text;
+	// [ws] TODO handle [[...|...|...]] syntax
+	for (let i = 0; i < parameters.length; i++) {
+		result = result.replaceAll('%' + (i + 1), '' + parameters[i]);
+	}
+	return result;
+}
+
 /** Returns a translation with a translation <span> around it. */
-export function span_(key: string): string {
-	return '<span class="translation" data-key="' + key + '">' + _(key) + '</span>';
+export function span_(key: string, ...parameters: number[]): string {
+	return '<span class="translation" data-key="' + key + '"' +
+		parameters.map((n: number, i: number) => ' data-parameter-' + i + '="' + n + '"').join()
+		+ '>' + _(key, ...parameters) + '</span>';
 }
 
 function escapeHTMLAttribute(attribute: string) {
--- test/translations.ts
+++ test/translations.ts
@@ -1,23 +1,65 @@
 import { describe, test } from 'node:test';
 import assert from 'node:assert';
 
-import * as translations from '../src/translations';
+import { setLanguage, _, PluralPattern } from '../src/translations';
 
 describe('UI translations', () => {
 
 	describe('the main translation function _', () => {
 		test('outputs in the currently set language', (t) => {
-			translations.setLanguage('en');
-			assert.strictEqual(translations._('language'), 'English');
-			translations.setLanguage('de');
-			assert.strictEqual(translations._('language'), 'Deutsch');
+			setLanguage('en');
+			assert.strictEqual(_('language'), 'English');
+			setLanguage('de');
+			assert.strictEqual(_('language'), 'Deutsch');
 		});
 
 		test('outputs in English if the string doesn\'t exist in the requested language', (t) => {
-			translations.setLanguage('en');
-			assert.strictEqual(translations._('language'), 'English');
-			translations.setLanguage('non-existing');
-			assert.strictEqual(translations._('language'), 'English');
+			setLanguage('en');
+			assert.strictEqual(_('language'), 'English');
+			setLanguage('non-existing');
+			assert.strictEqual(_('language'), 'English');
+		});
+	});
+
+	describe('plurality patterns', () => {
+		test('generating checkers', (t) => {
+			assert.deepEqual(new PluralPattern('n == 1').checkerFor(0), ['n', 1, '==']);
+			assert.deepEqual(new PluralPattern('n != 1').checkerFor(0), ['n', 1, '!=']);
+			assert.deepEqual(new PluralPattern('n > 1').checkerFor(0), ['n', 1, '>']);
+			assert.deepEqual(new PluralPattern('n >= 1').checkerFor(0), ['n', 1, '>=']);
+			assert.deepEqual(new PluralPattern('n < 1').checkerFor(0), ['n', 1, '<']);
+			assert.deepEqual(new PluralPattern('n <= 1').checkerFor(0), ['n', 1, '<=']);
+
+			assert.deepEqual(new PluralPattern('n == 42').checkerFor(0), ['n', 42, '==']);
+			assert.deepEqual(new PluralPattern('n == 42 % 3').checkerFor(0), ['n', 42, 3, '%', '==']);
+			assert.deepEqual(new PluralPattern('n % 7 == 3').checkerFor(0), ['n', 7, '%', 3, '==']);
+
+			assert.deepEqual(new PluralPattern('n > 13 && n % 8 < 4').checkerFor(0), ['n', 13, '>', 'n', 8, '%', 4, '<', '&&']);
+
+			assert.deepEqual(new PluralPattern('n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20)').checkerFor(0),
+				['n', 10, '%', 2, '>=', 'n', 10, '%', 4, '<=', '&&', 'n', 100, '%', 10, '<', 'n', 100, '%', 20, '>=', '||', '&&']);
+		});
+
+		test('evaluating for number inputs', (t) => {
+			let polishPattern = new PluralPattern('n == 1; n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20)');
+			assert.strictEqual(polishPattern.evaluate(0), 2);
+			assert.strictEqual(polishPattern.evaluate(1), 0);
+			assert.strictEqual(polishPattern.evaluate(2), 1);
+			assert.strictEqual(polishPattern.evaluate(3), 1);
+			assert.strictEqual(polishPattern.evaluate(4), 1);
+			assert.strictEqual(polishPattern.evaluate(5), 2);
+			assert.strictEqual(polishPattern.evaluate(10), 2);
+			assert.strictEqual(polishPattern.evaluate(11), 2);
+			assert.strictEqual(polishPattern.evaluate(12), 2);
+			assert.strictEqual(polishPattern.evaluate(13), 2);
+			assert.strictEqual(polishPattern.evaluate(14), 2);
+			assert.strictEqual(polishPattern.evaluate(15), 2);
+			assert.strictEqual(polishPattern.evaluate(20), 2);
+			assert.strictEqual(polishPattern.evaluate(21), 2);
+			assert.strictEqual(polishPattern.evaluate(22), 1);
+			assert.strictEqual(polishPattern.evaluate(23), 1);
+			assert.strictEqual(polishPattern.evaluate(24), 1);
+			assert.strictEqual(polishPattern.evaluate(25), 2);
 		});
 	});
 });
